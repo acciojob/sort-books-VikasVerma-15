1e1884ea1425d50f59eb5ec8a2fc031242717614
Vikas Verma <vikasverma15@gmail.com>
2026-01-07 18:32:55 +0530

    update list

--- src/components/BooksList.js
+++ src/components/BooksList.js
@@ -1,65 +1,69 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { setSortBy, setOrder } from "../redux/booksSlice";
-
-const fallbackBooks = [];
+import { fetchBooks, setSort } from "../redux/booksSlice";
 
 const BooksList = () => {
   const dispatch = useDispatch();
+  const { books, sortBy, order } = useSelector((state) => state.books);
 
-  const booksState = useSelector((state) => state.books || {});
-
-  const {
-    books = fallbackBooks,
-    sortBy = "title",
-    order = "asc"
-  } = booksState;
+  useEffect(() => {
+    dispatch(fetchBooks());
+  }, [dispatch]);
 
   const sortedBooks = [...books].sort((a, b) => {
-    if (a[sortBy] < b[sortBy]) return order === "asc" ? -1 : 1;
-    if (a[sortBy] > b[sortBy]) return order === "asc" ? 1 : -1;
+    const aVal = a[sortBy].toLowerCase();
+    const bVal = b[sortBy].toLowerCase();
+
+    if (aVal < bVal) return order === "asc" ? -1 : 1;
+    if (aVal > bVal) return order === "asc" ? 1 : -1;
     return 0;
   });
 
   return (
     <div>
-      {/* ✅ REQUIRED BY TEST */}
       <h1>Books List</h1>
 
-      {/* ✅ FIRST SELECT */}
+      {/* Sort by */}
+      <label>Sort by:</label>
       <select
         value={sortBy}
-        onChange={(e) => dispatch(setSortBy(e.target.value))}
+        onChange={(e) =>
+          dispatch(setSort({ sortBy: e.target.value, order }))
+        }
       >
         <option value="title">Title</option>
         <option value="author">Author</option>
-        <option value="year">Year</option>
+        <option value="publisher">Publisher</option>
       </select>
 
-      {/* ✅ SECOND SELECT */}
+      {/* Order */}
+      <label>Order:</label>
       <select
         value={order}
-        onChange={(e) => dispatch(setOrder(e.target.value))}
+        onChange={(e) =>
+          dispatch(setSort({ sortBy, order: e.target.value }))
+        }
       >
         <option value="asc">Ascending</option>
         <option value="desc">Descending</option>
       </select>
 
-      {/* ✅ TABLE REQUIRED */}
       <table>
         <thead>
           <tr>
             <th>Title</th>
             <th>Author</th>
-            <th>Year</th>
+            <th>Publisher</th>
+            <th>ISBN</th>
           </tr>
         </thead>
         <tbody>
-          {sortedBooks.map((book) => (
-            <tr key={book.id}>
+          {sortedBooks.map((book, index) => (
+            <tr key={index}>
               <td>{book.title}</td>
               <td>{book.author}</td>
-              <td>{book.year}</td>
+              <td>{book.publisher}</td>
+              <td>{book.isbn}</td>
             </tr>
           ))}
         </tbody>
@@ -69,4 +73,5 @@ const BooksList = () => {
 };
 
 export default BooksList;
+
 

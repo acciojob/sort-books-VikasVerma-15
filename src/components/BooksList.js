8eeb6eeb9362f6da180ee77c7fa280534ff31667
Vikas Verma <vikasverma15@gmail.com>
2026-01-07 18:36:53 +0530

    a

--- src/components/BooksList.js
+++ src/components/BooksList.js
@@ -1,14 +1,12 @@
 import React, { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { connect } from "react-redux";
 import { fetchBooks, setSort } from "../redux/booksSlice";
 
-const BooksList = () => {
-  const dispatch = useDispatch();
-  const { books, sortBy, order } = useSelector((state) => state.books);
+const BooksList = ({ books, sortBy, order, fetchBooks, setSort }) => {
 
   useEffect(() => {
-    dispatch(fetchBooks());
-  }, [dispatch]);
+    fetchBooks();
+  }, [fetchBooks]);
 
   const sortedBooks = [...books].sort((a, b) => {
     const aVal = a[sortBy].toLowerCase();
@@ -23,26 +21,20 @@ const BooksList = () => {
     <div>
       <h1>Books List</h1>
 
-      {/* Sort by */}
-      <label>Sort by:</label>
+      <label>Sort by</label>
       <select
         value={sortBy}
-        onChange={(e) =>
-          dispatch(setSort({ sortBy: e.target.value, order }))
-        }
+        onChange={(e) => setSort({ sortBy: e.target.value, order })}
       >
         <option value="title">Title</option>
         <option value="author">Author</option>
         <option value="publisher">Publisher</option>
       </select>
 
-      {/* Order */}
-      <label>Order:</label>
+      <label>Order</label>
       <select
         value={order}
-        onChange={(e) =>
-          dispatch(setSort({ sortBy, order: e.target.value }))
-        }
+        onChange={(e) => setSort({ sortBy, order: e.target.value })}
       >
         <option value="asc">Ascending</option>
         <option value="desc">Descending</option>
@@ -72,6 +64,12 @@ const BooksList = () => {
   );
 };
 
-export default BooksList;
+const mapStateToProps = (state) => ({
+  books: state.books.books,
+  sortBy: state.books.sortBy,
+  order: state.books.order,
+});
+
+export default connect(mapStateToProps, { fetchBooks, setSort })(BooksList);
 
 
